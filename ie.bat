a1e5622d0a508b8e282b704a31541450f058f206
Rob Ferguson <rjferguson21@gmail.com>
2015-07-07 14:52:33 -0500

    update selenium-server-standalone

--- ie.bat
+++ ie.bat
@@ -1,5 +1,5 @@
-echo WIN7 Machine - IE9
+echo WIN7 Machine - IE11
 set HUBURL="http://192.168.88.239:49044/grid/register"
-set BROWSERCAPS="maxInstances=1,browserName=internet explorer,version=9"
-java -jar selenium-server-standalone-2.42.2.jar -Dwebdriver.ie.driver="C:\Users\IEUser\AppData\Roaming\npm\node_modules\protractor\selenium\IEDriverServer.exe" -role node -hub %HUBURL% -port 4408 -maxSession 1 -browser %BROWSERCAPS%
+set BROWSERCAPS="maxInstances=1,browserName=internet explorer,version=11"
+java -jar selenium-server-standalone-2.46.0.jar -Dwebdriver.ie.driver="IEDriverServer.exe" -role node -hub %HUBURL% -port 4408 -maxSession 1 -browser %BROWSERCAPS%
 @pause
